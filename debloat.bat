e3f3b9eec8bc73d5cadc89eac73eceff2ab347fa
Rk Sharma <69055337+rks784262@users.noreply.github.com>
2024-10-07 11:26:02 +0530

    Update debloat.bat

--- debloat.bat
+++ debloat.bat
@@ -1,6 +1,6 @@
 @echo off
-::Clean and Optimize Windows PC
-::Backup current registry settings
+:: Clean and Optimize Windows PC
+:: Backup current registry settings
 reg export "HKCU\Control Panel\Accessibility" "%temp%\Accessibility_Backup.reg" /y
 reg export "HKCU\Control Panel\Keyboard" "%temp%\Keyboard_Backup.reg" /y
 reg export "HKEY_CURRENT_USER\Control Panel\Mouse" "%temp%\Mouse_Backup.reg" /y
@@ -68,7 +68,7 @@ reg add "HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Services\gupdate" /v "Start
 reg add "HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Services\gupdatem" /v "Start" /t reg_DWORD /d "4" /f
 reg add "HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Services\MSiSCSI" /v "Start" /t reg_DWORD /d "4" /f
 reg add "HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Services\TermService" /v "Start" /t reg_DWORD /d "4" /f
-reg add "HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Services\WSearch" /v "Start" /t reg_DWORD /d "4" /f
+reg add "HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Services\wuauserv" /v "Start" /t reg_DWORD /d "4" /f
 reg add "HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Services\WMPNetworkSvc" /v "Start" /t reg_DWORD /d "4" /f
 
 :: Adjust Internet Explorer Security Settings
@@ -76,7 +76,12 @@ reg add "HKEY_CURRENT_USER\Software\Microsoft\Internet Explorer\Main" /v "Disabl
 reg add "HKEY_CURRENT_USER\Software\Microsoft\Internet Explorer\Main" /v "Disable Script Debugging (Other)" /t reg_SZ /d "yes" /f
 reg add "HKEY_CURRENT_USER\Software\Microsoft\Internet Explorer\Main" /v "Error DIALOGS" /t reg_SZ /d "no" /f
 
+:: Run Windows Troubleshooter
+echo Running Windows Troubleshooter...
+start ms-settings:troubleshoot
+
 :: Restart Windows Explorer to apply changes
+echo Restarting Windows Explorer...
 taskkill /f /im explorer.exe
 start explorer.exe
 
