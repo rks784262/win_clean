c505c3661c98b76ae441ad51b9b2ad27e935a6b6
Rk Sharma <69055337+rks784262@users.noreply.github.com>
2024-10-07 11:40:53 +0530

    Update debloat.bat

--- debloat.bat
+++ debloat.bat
@@ -67,7 +67,9 @@ reg add "HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Services\wersvc" /v "Start"
 reg add "HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Services\gupdate" /v "Start" /t reg_DWORD /d "4" /f
 reg add "HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Services\gupdatem" /v "Start" /t reg_DWORD /d "4" /f
 reg add "HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Services\MSiSCSI" /v "Start" /t reg_DWORD /d "4" /f
-reg add "HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Services\TermService" /v "Start" /t reg_DWORD /d "4" /f
+reg add "HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Services\SystemEventsBroker" /v "Start" /t reg_DWORD /d "4" /f
+reg add "HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Services\MSDTC" /v "Start" /t reg_DWORD /d "4" /f
+reg add "HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Services\VMCompute" /v "Start" /t reg_DWORD /d "4" /f
 reg add "HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Services\wuauserv" /v "Start" /t reg_DWORD /d "4" /f
 reg add "HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Services\WMPNetworkSvc" /v "Start" /t reg_DWORD /d "4" /f
 
@@ -79,6 +81,60 @@ reg add "HKEY_CURRENT_USER\Software\Microsoft\Internet Explorer\Main" /v "Error 
 :: Run Windows Troubleshooter
 echo Running Windows Troubleshooter...
 start ms-settings:troubleshoot
+
+:: Cleanup Temporary Files and Optimize System
+echo Performing additional optimizations and cleanup...
+del /q /s "%temp%\*.*"
+netsh winsock reset
+sfc /scannow
+nvidia-smi -rac
+powercfg /duplicatescheme e9a42b02-d5df-448d-aa00-03f14749eb61
+echo. | set /p dummyVar="Releasing unused memory..."
+EmptyWorkingSet(HANDLE(-1))
+defrag /C /O
+
+echo All optimizations and cleanup complete.
+
+:: Delete unnecessary temp files
+del /s /f /q c:\windows\temp\*.*
+rd /s /q c:\windows\temp
+md c:\windows\temp
+del /s /f /q %temp%\*.*
+rd /s /q %temp%
+md %temp%
+del /s /f /q c:\windows\tempor~1
+del /s /f /q c:\windows\temp
+del /s /f /q c:\windows\tmp
+del /s /f /q c:\windows\ff*.tmp
+del /s /f /q c:\windows\history
+del /s /f /q c:\windows\cookies
+del /s /f /q c:\windows\recent
+del /s /f /q c:\windows\spool\printers
+del /s /f /q %userprofile%\Recent\*.*
+del /s /f /q C:\Windows\Prefetch\*.*
+del /s /f /q C:\Windows\Temp\*.*
+del /s /f /q %USERPROFILE%\appdata\local\temp\*.*
+del /Q C:\Users\%username%\AppData\Local\Microsoft\Windows\INetCache\IE\*.*
+del /Q C:\Windows\Downloaded Program Files\*.*
+rd /s /q %SYSTEMDRIVE%\$Recycle.bin
+del /Q C:\Users\%username%\AppData\Local\Temp\*.*
+del /Q C:\Windows\Temp\*.*
+del /Q C:\Windows\Prefetch\*.*
+del /s /f /q %SystemRoot%\setupapi.log
+del /s /f /q %SystemRoot%\Panther\*
+del /s /f /q %SystemRoot%\inf\setupapi.app.log
+del /s /f /q %SystemRoot%\inf\setupapi.dev.log
+del /s /f /q %SystemRoot%\inf\setupapi.offline.log
+
+:: Remove User-Specific Registry Entries
+echo Removing non-essential user-specific registry entries...
+reg delete "HKCU\Software\Microsoft\Windows\CurrentVersion\Explorer\RecentDocs" /va /f
+reg delete "HKCU\Software\Microsoft\Windows\CurrentVersion\Explorer\RunMRU" /va /f
+reg delete "HKCU\Software\Microsoft\Windows\CurrentVersion\Explorer\ComDlg32\OpenSaveMRU" /va /f
+reg delete "HKCU\Software\Microsoft\Windows\CurrentVersion\Explorer\ComDlg32\LastVisitedPidlMRU" /va /f
+reg delete "HKCU\Software\Microsoft\Windows\CurrentVersion\Explorer\ComDlg32\LastVisitedPidlMRULegacy" /va /f
+reg delete "HKCU\Software\Microsoft\MediaPlayer\Player\RecentFileList" /va /f
+reg delete "HKCU\Software\Microsoft\MediaPlayer\Player\RecentURLList" /va /f
 
 :: Restart Windows Explorer to apply changes
 echo Restarting Windows Explorer...
