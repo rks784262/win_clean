59749666ff500357ed132320c74b173d44e239e8
Rk Sharma <69055337+rks784262@users.noreply.github.com>
2024-10-08 08:34:50 +0530

    Update internet.bat

--- internet.bat
+++ internet.bat
@@ -1,48 +1,6 @@
-@echo off
-echo Starting Internet Booster...
-
-rem Navigate to root directory
-cd \
-
-rem Show current TCP settings
-echo.
-echo Current TCP Global Settings:
+ cd\
 netsh int tcp show global
-
-rem Enable TCP Chimney Offload
-echo.
-echo Enabling TCP Chimney Offload...
 netsh int tcp set global chimney=enabled
-
-rem Disable TCP Heuristics
-echo.
-echo Disabling TCP Heuristics...
 netsh int tcp set heuristics disabled
-
-rem Set TCP Auto-Tuning Level to Normal
-echo.
-echo Setting TCP Auto-Tuning Level to Normal...
 netsh int tcp set global autotuninglevel=normal
-
-rem Set TCP Congestion Provider to CTCP using the supplemental command
-echo.
-echo Setting TCP Congestion Provider to CTCP...
-netsh int tcp set supplemental congestionprovider=ctcp
-
-rem Display the new settings
-echo.
-echo Updated TCP Global Settings:
-netsh int tcp show global
-
-:: Detect the system architecture
-if "%PROCESSOR_ARCHITECTURE%"=="AMD64" (
-    echo.
-    echo This is a 64-bit system.
-) else (
-    echo.
-    echo This is a 32-bit system.
-)
-
-echo.
-echo Internet Booster Completed. Please restart your computer for changes to take effect.
-pause
+netsh int tcp set global congestionprovider=ctcp
